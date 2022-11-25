20ea6a2321910079046b585128f7c60f043d2c10
Anirudh <116883902+Anirudh-Chandran@users.noreply.github.com>
2022-11-25 11:33:41 +0000

    Update CA1.py

--- CA1.py
+++ CA1.py
@@ -1,5 +1,5 @@
-class Employee(name, weeklyhours, rate, overtimeRate, weeklytaxcredit):
-    def __init__(self):
+class Employee:
+    def __init__(self,name, weeklyhours, rate, overtimeRate, weeklytaxcredit):
         self.name = name
         self.weeklyhours= weeklyhours
         self.rate = rate
@@ -12,10 +12,19 @@ class Employee(name, weeklyhours, rate, overtimeRate, weeklytaxcredit):
     def computeWeeklyPay (self,hours):
         gross_pay = 0
         gross_pay += (self.rate*hours) + self.weeklytaxcredit
+        if gross_pay <0:
+            return 0
         return gross_pay
 
     def computeTax(self,grossPay):
         tax_due = 0
-        tax_due += grossPay * 0.4
+        tax_due += (grossPay * 0.4) - self.weeklytaxcredit
+        if tax_due < 0:
+            print("Tax value cannot be negative")
+            return 0
         return tax_due
+
+
+e1 = Employee
+
 
